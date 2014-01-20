7994b806bad848458c36ad96d2a1a6a5c6deb48e
George Fourtounis <gfour@softlab.ntua.gr>
2014-01-20 19:35:36 +0200

    Added parallel map example.

---
+++ Examples/Parallel/Parmap.hs
@@ -0,0 +1,19 @@
+-- | Parallel map example.
+module Parmap where
+
+import Control.Parallel (par, pseq)
+
+result :: Int
+result = sum2 (parmap fib (ints 30))
+
+ints :: Int -> [Int]
+ints i = if i==0 then [] else i:(ints (i-1))
+
+fib :: Int -> Int
+fib x = if x<2 then 1 else ((fib (x-1)) + (fib (x-2)))
+
+sum2 :: [Int] -> Int
+sum2 l = case l of [] -> 0 ; x:xs -> x+(sum2 xs)
+
+parmap :: (Int -> Int) -> [Int] -> [Int]
+parmap f l = case l of [] -> [] ; x:xs -> let z = (f x) in z `par` (z:(parmap f xs))
