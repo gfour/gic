cbc1923038ce98ffa5a0104f35ce706255f2281d
George Fourtounis <gfour@softlab.ntua.gr>
2014-01-20 22:51:58 +0200

    Fixed module name and cutoff in Parfib.

--- Examples/Parallel/Parfib.hs
+++ Examples/Parallel/Parfib.hs
@@ -1,14 +1,14 @@
--- | Parallel Fibonacci example, parallelism cutoff at parameter 14.
+-- | Parallel Fibonacci example, parallelism cutoff at parameter 11.
 
-module Fibpar where
+module Parfib where
 
 import Control.Parallel (par, pseq)
 
 result :: Int
-result = fib 24
+result = fib 31
 
 fib :: Int -> Int
-fib x = if x<2 then 1 else if x<14 then ((fib (x-1)) + (fib (x-2))) else addpar (fib (x-1)) (fib (x-2)) ;
+fib x = if x<2 then 1 else if x<11 then ((fib (x-1)) + (fib (x-2))) else addpar (fib (x-1)) (fib (x-2)) ;
 
 addpar :: Int -> Int -> Int
 addpar a b = (a `par` b) `pseq` (a+b)
