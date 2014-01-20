3e5ba00ead6c422ccfed7d4d7476a8f5aa64917a
George Fourtounis <gfour@softlab.ntua.gr>
2014-01-20 23:05:41 +0200

    Updated Parmap to do correct memoization.

--- Examples/Parallel/Parmap.hs
+++ Examples/Parallel/Parmap.hs
@@ -4,7 +4,7 @@ module Parmap where
 import Control.Parallel (par, pseq)
 
 result :: Int
-result = sum2 (parmap fib (ints 30))
+result = sum2 (parmap fib (ints 33))
 
 ints :: Int -> [Int]
 ints i = if i==0 then [] else i:(ints (i-1))
@@ -16,4 +16,10 @@ sum2 :: [Int] -> Int
 sum2 l = case l of [] -> 0 ; x:xs -> x+(sum2 xs)
 
 parmap :: (Int -> Int) -> [Int] -> [Int]
-parmap f l = case l of [] -> [] ; x:xs -> let z = (f x) in z `par` (z:(parmap f xs))
+parmap f l =
+  case l of
+    []   -> []
+    x:xs -> auxpar (f x) (parmap f xs)
+
+auxpar :: Int -> [Int] -> [Int]
+auxpar z fxs = (z `par` fxs) `pseq` (z:fxs)
