4b2bf1b4a1d1d43085b4c8d3a2d152e13dd1e109
George Fourtounis <gfour@softlab.ntua.gr>
2014-02-18 15:33:08 +0200

    Changed queens.hs to use the already defined list data type.

--- Examples/NewBench/queens.hs
+++ Examples/NewBench/queens.hs
@@ -12,14 +12,13 @@ result = count size
 count :: Int -> Int
 count n = length2 (queens n)
 
-data List = Nil | Cons Int List
-data LList = LNil | LCons List LList
+data LList = LNil | LCons [Int] LList
 
 queens :: Int -> LList
 queens n = gen n n
 
 gen :: Int -> Int -> LList
-gen nq n = if n == 0 then LCons Nil LNil else gen_1 nq (gen nq (n-1))
+gen nq n = if n == 0 then LCons [] LNil else gen_1 nq (gen nq (n-1))
 
 gen_1 :: Int -> LList -> LList
 gen_1 nq bs =
@@ -27,20 +26,20 @@ gen_1 nq bs =
     LNil -> LNil
     LCons b bs' -> gen_2 nq b 1 (gen_1 nq bs')
 
-gen_2 :: Int -> List -> Int -> LList -> LList
+gen_2 :: Int -> [Int] -> Int -> LList -> LList
 gen_2 nq b q rest = if q <= nq then
                       if safe q 1 b then
-                        LCons (Cons q b) (gen_2 nq b (q+1) rest)
+                        LCons (q:b) (gen_2 nq b (q+1) rest)
                       else
                         gen_2 nq b (q+1) rest
                     else
                       rest
 
-safe :: Int -> Int -> List -> Bool
+safe :: Int -> Int -> [Int] -> Bool
 safe x d qs =
   case qs of
-    Nil -> True
-    Cons q l -> (x /= q) && (x /= (q+d)) && (x /= (q-d)) && (safe x (d+1) l)
+    []  -> True
+    q:l -> (x /= q) && (x /= (q+d)) && (x /= (q-d)) && (safe x (d+1) l)
 
 length2 :: LList -> Int
 length2 bs =
