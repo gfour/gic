6655e89068307625fa719411b3b7c2b1c5beddb0
George Fourtounis <gfour@softlab.ntua.gr>
2014-02-20 02:53:48 +0200

    Updated Doxygen documentation.

--- c/gc.h
+++ c/gc.h
@@ -1,29 +1,27 @@
-/*
-  GC macros.
-*/
+/** @file gc.h
 
-// Enables internal consistency check (used for debugging)
-#define DEBUG_GC 0
+  GC macros.
+
+ */
+
+/** Enables internal consistency check (used for debugging). */
+#define DEBUG_GC 1
 // Print (lots of) diagnostic messages
 // #define VERBOSE_GC
-// Print statistics
+/** Print statistics. */
 #define GC_STATS 1
 
-// GC assertions
+/** GC assertion. */
 #if DEBUG_GC
 #include <assert.h>
 #define ASSERT_GC(c, m)      assert((c) && (m))
 #else
 #define ASSERT_GC(n, m)      do { } while(0)
 #endif
+/** GC assertion (used as an expression). */
 #define ASSERT_GC_EXPR(c, m) ({ ASSERT_GC(c, m); })
 
-// The size of the header prepended to every allocated memory area.
-#define MEM_HEADER_SZ (sizeof(size_t))
-
-// Check if a pointer belongs to one of the two spaces
+/** Check if a pointer belongs to space 1. */
 #define ISSPACE1(x) ((x >= space1) && (x <= space1 + MAXMEMSPACE))
+/** Check if a pointer belongs to space 2. */
 #define ISSPACE2(x) ((x >= space2) && (x <= space2 + MAXMEMSPACE))
-
-#define MAGIC     ((unsigned long) 0xbad0bad1bad6bad7)
-#define FORWARDED ((unsigned long) 0xbad0bad1bad6bad5)
