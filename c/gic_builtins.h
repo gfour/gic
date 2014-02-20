ca9635b46d33bf77d9d99562280fbb477d9e8142
George Fourtounis <gfour@softlab.ntua.gr>
2014-02-20 02:57:59 +0200

    Added Doxygen documentation.

--- c/gic_builtins.h
+++ c/gic_builtins.h
@@ -1,6 +1,18 @@
-/* The built-in functions of the GIC runtime. */
+/** @file gic_builtins.h
 
-// Integer multiplication
+   The built-in functions of the GIC runtime.
+
+*/
+
+/** Big integer multiplication using libgmp.
+    \param a The first big integer.
+    \param b The second big integer.
+    \return The product of a and b.
+*/
 Susp mulI(Susp a, Susp b);
-// Convert C strings to GIC lists
+/** Converts C strings to GIC lists.
+    \param str The C string to convert.
+    \param chars The length of the C string.
+    \return A GIC string (i.e. a lazy list of characters).
+*/
 Susp strToList(char *str, int chars, TP_ T0);
