e8dbc8e7cfbfff649054dd5b034a7ebcc382ae4b
George Fourtounis <gfour@softlab.ntua.gr>
2014-03-04 22:04:44 +0200

    Code cleanup.

--- SLIC/Driver.hs
+++ SLIC/Driver.hs
@@ -67,171 +67,169 @@ import SLIC.Front.LLifter.Lifter
 -- 
 processFL :: Options -> [DFI] -> ModF -> IO (Maybe DFI)
 processFL opts dfis inputModule =
-    do
-      -- ---------------- FL ---------------------
-      -- fix syntactic restrictions
-      let p0Syntax = procModSource fixSyntax inputModule
-      -- a-rename variables (and forget module information)
-      let p0Alpha = uniqueNames p0Syntax
-      -- rename functions that clash with built-in functions and preprocess
-      -- code that may have come from the GHC front-end
-      let p0Preproc = procModSource (\_ p -> convertFromGHC p) p0Alpha
-      -- do datatypes-as-functions transformation
-      let p0Constrs = constrToFuncs opts p0Preproc
-      -- _ <- (putStrLn ("-- * Datatypes-as-functions in modules") >> printLn p0Constrs)
-      -- check that the modules are now in good shape for the lambda lifter
-      let p0Checked = checkMod p0Constrs
-      -- lift let bindings
-      let p0Lifted = lambdaLiftMod opts p0Checked
-      -- final preprocessed source
-      let p1 = p0Lifted
-      -- _ <- (putStrLn ("-- * Original") >> printLn inputModule)
-      -- _ <- (putStrLn ("-- * Syntax") >> printLn p0Syntax)
-      -- _ <- (putStrLn ("-- * Alpha") >> printLn p0Alpha)
-      -- _ <- (putStrLn ("-- * Lifted") >> printLn p0Lifted)
-      -- _ <- (putStrLn ("-- * Preprocessed modules") >> printLn p1)
+  do -- ---------------- FL ---------------------
+     -- fix syntactic restrictions
+     let p0Syntax = procModSource fixSyntax inputModule
+     -- a-rename variables (and forget module information)
+     let p0Alpha = uniqueNames p0Syntax
+     -- rename functions that clash with built-in functions and preprocess
+     -- code that may have come from the GHC front-end
+     let p0Preproc = procModSource (\_ p -> convertFromGHC p) p0Alpha
+     -- do datatypes-as-functions transformation
+     let p0Constrs = constrToFuncs opts p0Preproc
+     -- _ <- (putStrLn ("-- * Datatypes-as-functions in modules") >> printLn p0Constrs)
+     -- check that the modules are now in good shape for the lambda lifter
+     let p0Checked = checkMod p0Constrs
+     -- lift let bindings
+     let p0Lifted = lambdaLiftMod opts p0Checked
+     -- final preprocessed source
+     let p1 = p0Lifted
+     -- _ <- (putStrLn ("-- * Original") >> printLn inputModule)
+     -- _ <- (putStrLn ("-- * Syntax") >> printLn p0Syntax)
+     -- _ <- (putStrLn ("-- * Alpha") >> printLn p0Alpha)
+     -- _ <- (putStrLn ("-- * Lifted") >> printLn p0Lifted)
+     -- _ <- (putStrLn ("-- * Preprocessed modules") >> printLn p1)
 
-      -- run type inference on each module
-      e1 <-
-        case optTC opts of
-          GICTypeInf useAnnot -> typeInferMod useAnnot p1
-          GHCTypeInf          -> readTypeSigs p1
+     -- run type inference on each module
+     e1 <-
+       case optTC opts of
+         GICTypeInf useAnnot -> typeInferMod useAnnot p1
+         GHCTypeInf          -> readTypeSigs p1
 
-      -- merge all typing environments (single environment used by some back-ends)
-      -- let e1 = unions modEnvs
+     -- merge all typing environments (single environment used by some back-ends)
+     -- let e1 = unions modEnvs
 
-      -- sanity check for FL
-      let ok = isValidFL p0Lifted e1
+     -- sanity check for FL
+     let ok = isValidFL p0Lifted e1
       
-      -- let modsAndEnvs = zip p1 modEnvs
+     -- let modsAndEnvs = zip p1 modEnvs
           
-      -- defunctionalize module and create accompanying DFI
-      let dfModF = defuncMod opts e1 p1
-      -- _ <- printLn dfMods
-      -- do the -enum opimization
-      let dfModFFinal = optEnums opts dfModF
+     -- defunctionalize module and create accompanying DFI
+     let dfModF = defuncMod opts e1 p1
+     -- _ <- printLn dfMods
+     -- do the -enum opimization
+     let dfModFFinal = optEnums opts dfModF
+     
+     -- get the defunctionalized FL modules and defunctionalization interfaces
+     let ModD p0Def p0DefDfi = dfModFFinal    
+     -- _ <- (putStrLn ("* Defunctionalized") >> printLn p0Def)
       
-      -- get the defunctionalized FL modules and defunctionalization interfaces
-      let ModD p0Def p0DefDfi = dfModFFinal    
-      -- _ <- (putStrLn ("* Defunctionalized") >> printLn p0Def)
+     -- get the total environment
+     let env = dfiTEnv $ p0DefDfi
       
-      -- get the total environment
-      let env = dfiTEnv $ p0DefDfi
-      
-      -- enable strictness mode if set with the command-line switch
-      let p0DefStr = procModSource (\_ p->markStrict (optStrict opts) p) p0Def
-      -- _ <- (putStrLn ("* Strictness marked") >> printLn p0Def)          
-      -- enumerate case expressions and bound variables
-      let p0BVars = procModSource procBV p0DefStr
-      -- update the DFI with this new information
-      let p0Dfi = updPMDepths p0DefDfi p0BVars
-      -- _ <- (putStrLn ("* Bound variables processed") >> printLn p0BVars)
+     -- enable strictness mode if set with the command-line switch
+     let p0DefStr = procModSource (\_ p->markStrict (optStrict opts) p) p0Def
+     -- _ <- (putStrLn ("* Strictness marked") >> printLn p0Def)          
+     -- enumerate case expressions and bound variables
+     let p0BVars = procModSource procBV p0DefStr
+     -- update the DFI with this new information
+     let p0Dfi = updPMDepths p0DefDfi p0BVars
+     -- _ <- (putStrLn ("* Bound variables processed") >> printLn p0BVars)
           
-      -- inline type classes for statically known instances
-      let tcInfo = mergeTcInfos $ [builtinTcInfo, modTCs p0BVars]
-      let p0Tc = inlineTcMethods tcInfo env p0BVars
+     -- inline type classes for statically known instances
+     let tcInfo = mergeTcInfos $ [builtinTcInfo, modTCs p0BVars]
+     let p0Tc = inlineTcMethods tcInfo env p0BVars
       
-      -- final preprocessed and defunctionalized source to be used
-      let p0Final = p0Tc
-      -- _ <- printLn p0Final
+     -- final preprocessed and defunctionalized source to be used
+     let p0Final = p0Tc
+     -- _ <- printLn p0Final
       
-      -- do variable usage analysis
-      let cbnVars = findCBNVars p0Final
-      let stricts = gatherStrictVars p0Final
-      -- let mergedVarUsage = (cbnVars, stricts)
+     -- do variable usage analysis
+     let cbnVars = findCBNVars p0Final
+     let stricts = gatherStrictVars p0Final
+     -- let mergedVarUsage = (cbnVars, stricts)
           
-      if not ok then
-        do putStrLn "Invalid input program."
-           return Nothing
-      else
-        case optAction opts of 
-          ACheck ->
-            do putStrLn "Valid program"
-               return Nothing
-          -- program printers for FL, preprocessed FL, defunctionalized code,
-          -- and the final program that is fed to the intensional transformation
-          APrintFL -> printLn p1
-          APrintFLPre -> printLn p0Final
-          APrintDF ->
-            (if optVerbose opts then
-               putStrLn "== Environment ==" >>
-               putStrLn (pprintE env "")
-             else return ()) >>
-            case optCMode opts of
-                CompileModule ->
-                  putStrLn "== Separately defunctionalized module ==" >>
-                  printLn p0Final
-                Whole ->
-                  let wholeProgFinal = linkF opts p0Final p0Dfi
-                  in  putStrLn "== Whole defunctionalized program ==" >>
-                      printLn wholeProgFinal
-          AEvalFL ->
-            case optCMode opts of
-                CompileModule ->
-                  error "The non-strict FL interpreter does not support separate compilation."
-                Whole ->
-                  let wholeProgFinal = linkF opts p0Final p0Dfi
-                  in  do evalFL wholeProgFinal
-                         return Nothing
-          APrintHIL1 -> printLn p1
-          -- environment printer
-          APrintEnv ->
-            do putStr (pprintE e1 "")
-               return Nothing
+     if not ok then
+       do putStrLn "Invalid input program."
+          return Nothing
+     else
+       case optAction opts of 
+         ACheck ->
+           do putStrLn "Valid program"
+              return Nothing
+         -- program printers for FL, preprocessed FL, defunctionalized code,
+         -- and the final program that is fed to the intensional transformation
+         APrintFL -> printLn p1
+         APrintFLPre -> printLn p0Final
+         APrintDF ->
+           (if optVerbose opts then
+              putStrLn "== Environment ==" >>
+              putStrLn (pprintE env "")
+            else return ()) >>
+           case optCMode opts of
+               CompileModule ->
+                 putStrLn "== Separately defunctionalized module ==" >>
+                 printLn p0Final
+               Whole ->
+                 let wholeProgFinal = linkF opts p0Final p0Dfi
+                 in  putStrLn "== Whole defunctionalized program ==" >>
+                     printLn wholeProgFinal
+         AEvalFL ->
+           case optCMode opts of
+               CompileModule ->
+                 error "The non-strict FL interpreter does not support separate compilation."
+               Whole ->
+                 let wholeProgFinal = linkF opts p0Final p0Dfi
+                 in  do evalFL wholeProgFinal
+                        return Nothing
+         APrintHIL1 -> printLn p1
+         -- environment printer
+         APrintEnv ->
+           do putStr (pprintE e1 "")
+              return Nothing
 
-          -- all other options need the intensional transformation to take place
-          _ ->
-            let -- translates program to the intermediate intensional language
-                mHIL    = fromHFtoHI p0Final
-                -- run the intensional transformation
-                pTrans  = itransM mHIL
-                -- convert the result to ZOIL
-                p3      = fromHItoZI pTrans
-            in  case optAction opts of
-
-                  -- program printers for the transformed program, the 0-order
-                  -- result program, and the TTD program
-                  APrintHIL2 -> printLn pTrans
-                  APrintZOIL -> printLn p3
-                  APrintTTD  ->
-                    do callTTDBackend opts p0Dfi p3
-                       return Nothing
+          -- All other options need the intensional transformation to take place.
+         _ ->
+           let -- Translate program to the intermediate intensional language.
+               mHIL    = fromHFtoHI p0Final
+               -- Do the intensional transformation.
+               pTrans  = itransM mHIL
+               -- Convert the result to ZOIL.
+               p3      = fromHItoZI pTrans
+           in  case optAction opts of
+                 -- Program printers for the transformed program, the 0-order
+                 -- result program, and the TTD program.
+                 APrintHIL2 -> printLn pTrans
+                 APrintZOIL -> printLn p3
+                 APrintTTD  ->
+                   do callTTDBackend opts p0Dfi p3
+                      return Nothing
 
                   -- generates the dataflow graph for the TTD program
-                  AGenerateDFG ->
-                     do callTTDBackend opts p0Dfi p3
-                        return Nothing
-
-                  -- interpreters: call-by-name, integrated lazy, Erlang-based
-                  AEvalZOILCBN ->
-                    do evalZOILCBN opts p0Dfi p3
-                       return Nothing
-                  AEvalZOILLazy ->
-                    do evalZOILLazy opts p0Dfi p3
-                       return Nothing
-                  AEvalErl ->
-                    case optCMode opts of
-                      CompileModule -> error "The Erlang-based interpreter does not support separate compilation."
-                      Whole ->
-                        let m    = fst $ modNameF p3
-                            code = optimize m $ modProg p3
-                        in  do callErlBackend m code opts
-                               return Nothing
-
-                  -- Call the Maude back-end.
-                  ACompileMaude ->
-                    do callMaudeBackend (modProg p3) opts
-                       return Nothing
-
-                  -- Call the TTD back-end.
-                  AEvalTTD ->
+                 AGenerateDFG ->
                     do callTTDBackend opts p0Dfi p3
                        return Nothing
 
-                  -- Call the LAR back-end.
-                  _  ->
-                    let cUnit = (p0Final, p3, p0Dfi, cbnVars, stricts)
-                    in  itransfLAR opts env cUnit
+                  -- interpreters: call-by-name, integrated lazy, Erlang-based
+                 AEvalZOILCBN ->
+                   do evalZOILCBN opts p0Dfi p3
+                      return Nothing
+                 AEvalZOILLazy ->
+                   do evalZOILLazy opts p0Dfi p3
+                      return Nothing
+                 AEvalErl ->
+                   case optCMode opts of
+                     CompileModule -> error "The Erlang-based interpreter does not support separate compilation."
+                     Whole ->
+                       let m    = fst $ modNameF p3
+                           code = optimize m $ modProg p3
+                       in  do callErlBackend m code opts
+                              return Nothing
+
+                  -- Call the Maude back-end.
+                 ACompileMaude ->
+                   do callMaudeBackend (modProg p3) opts
+                      return Nothing
+
+                 -- Call the TTD back-end.
+                 AEvalTTD ->
+                   do callTTDBackend opts p0Dfi p3
+                      return Nothing
+
+                 -- Call the LAR back-end.
+                 _  ->
+                   let cUnit = (p0Final, p3, p0Dfi, cbnVars, stricts)
+                   in  itransfLAR opts env cUnit
 
 -- | A compilation unit: preprocessed FL code, 0-order intensional code, 
 --   generated DFI, call-by-name/strictness information.
