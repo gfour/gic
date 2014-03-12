a5fb7b630d3b0ba2f83eceaa5b908add59900ea0
George Fourtounis <gfour@softlab.ntua.gr>
2014-03-12 15:39:43 +0200

    Added test for lists of negative integers.

---
+++ Examples/Data/neglist.hs
@@ -0,0 +1,5 @@
+-- | Tests the negation operator and pretty printing of negative lists
+--   (which check the value flag when -compact is on).
+
+result :: [Int]
+result = [-1, -2, -3, -4, -5, -7, -8, -9, -16, -17, -42]
