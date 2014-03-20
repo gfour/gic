0aafb5a16ea4876dfa8ae7e19369c54e61bb0fb4
George Fourtounis <gfour@softlab.ntua.gr>
2014-03-20 18:24:11 +0200

    Added space after semicolon.

--- SLIC/LAR/SMacrosAux.hs
+++ SLIC/LAR/SMacrosAux.hs
@@ -288,7 +288,7 @@ mkPUSHAR :: Bool -> ShowS
 mkPUSHAR dbg =
   let debug_PUSHAR =
         if dbg then
-          ("if (sstack_ptr >= sstack_bottom + SSTACK_MAX_SIZE) { printf(\"Pointer stack overflow.\\n\"); exit(EXIT_FAILURE); } ;"++)
+          ("if (sstack_ptr >= sstack_bottom + SSTACK_MAX_SIZE) { printf(\"Pointer stack overflow.\\n\"); exit(EXIT_FAILURE); } ; "++)
         else id
   in  ("// Record LAR pointer in the explicit pointer stack."++).nl.
       ("#define PUSHAR(a) ((TP_*)({ "++).debug_PUSHAR.
