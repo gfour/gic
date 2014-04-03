de7828642dec603405d162c4f4ca27348014ec0a
George Fourtounis <gfour@softlab.ntua.gr>
2014-04-04 00:38:00 +0300

    Better parameter handling in macros.

--- c/lar_semi.h
+++ c/lar_semi.h
@@ -6,14 +6,14 @@
 
 #ifdef GC
 /* LAR wrapper (*-operator). */
-#define AR_TP(tp)  *tp
+#define AR_TP(tp)  *(tp)
 /* LAR wrapper (&-operator). */
 #define AR_REF(tp) &(tp)
 #else
 /* Dummy LAR wrapper (*-operator). */
-#define AR_TP(tp)  tp
+#define AR_TP(tp)  (tp)
 /* Dummy LAR wrapper (&-operator). */
-#define AR_REF(tp) tp
+#define AR_REF(tp) (tp)
 #endif /* GC */
 
 /** The start of the ARGS fields, holding the code pointers of the thunks.
@@ -24,7 +24,8 @@
 #define THE_ARGS(T)   ((byte *) &((T)->data))
 #endif /* GC */
 
-/** Printer for activation records (used for debugging). */
+/** Printer for activation records (used for debugging).
+    \param a The LAR. This is used many times, it should not be a macro. */
 #ifdef GC
 #define DEBUG_PRINT_AR(a) { printf("%p@{ prev=%p, a=%d, n=%d } [size=%ld] ", a, AR_prev(a), ARITY(a), NESTING(a), AR_SIZE(a)); printf("thunks: "); int i; for (i=0; i<ARITY(a); i++) { printf("<"); MM_printThunk(i, a); ; printf("> "); }; }
 #else
